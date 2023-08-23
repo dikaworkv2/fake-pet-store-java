7e903153f200ae75c2de84b8a7cdf78e5e5a0dcf
Farhandika <18220015@std.stei.itb.ac.id>
2023-08-23 10:58:41 +0700

    Added logger

---
+++ src/main/java/com/example/petstore/controller/spawner/SpawnerController.java
@@ -0,0 +1,35 @@
+package com.example.petstore.controller.spawner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public final class SpawnerController {
+    private static final Logger logger = LoggerFactory.getLogger(SpawnerController.class);
+
+    @GetMapping("/spawn/info")
+    public String spawnLogInfo() {
+        logger.info("spawn info");
+        return "success";
+    }
+
+    @GetMapping("/spawn/warn")
+    public String spawnLogWarn() {
+        logger.warn("spawn warn");
+        return "success";
+    }
+
+    @GetMapping("/spawn/error")
+    public String spawnLogError() {
+        logger.error("spawn error");
+        return "success";
+    }
+
+    @GetMapping("/spawn/debug")
+    public String spawnLogDebug() {
+        logger.debug("spawn debug");
+        return "success";
+    }
+}
