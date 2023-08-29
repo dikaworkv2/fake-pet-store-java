1af5a685c6e0be92efa1a6ccdc9a5adf4959db68
Farhandika <18220015@std.stei.itb.ac.id>
2023-08-29 12:45:33 +0700

    make java pp complie to tomcat

--- src/main/java/com/example/petstore/FakepetstoreApplication.java
+++ src/main/java/com/example/petstore/FakepetstoreApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FakepetstoreApplication {
+public class FakepetstoreApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FakepetstoreApplication.class, args);
